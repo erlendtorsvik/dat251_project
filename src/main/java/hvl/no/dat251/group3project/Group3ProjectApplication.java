e6a71cf79a814dc2f9e36d178f1b490f9eae337f
Jaroslaw Pasiak <wojapa@gmail.com>
2021-02-23 14:52:31 +0100

    initial files

---
+++ src/main/java/hvl/no/dat251/group3project/Group3ProjectApplication.java
@@ -0,0 +1,13 @@
+package hvl.no.dat251.group3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hvl/no/dat251/group3project/Group3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.no.dat251.group3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
