1e7c7b1539208b0b96ac2b575bb28c2290952fbd
Your Name <you@example.com>
2021-03-05 18:37:34 +0100

    itemservicetest update cris suck!

--- src/test/java/hvl/no/dat251/group3project/ItemServiceTest.java
+++ src/test/java/hvl/no/dat251/group3project/ItemServiceTest.java
@@ -2,6 +2,7 @@ package hvl.no.dat251.group3project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,25 +12,79 @@ import hvl.no.dat251.group3project.entity.Item;
 import hvl.no.dat251.group3project.repositories.IItemRepository;
 import hvl.no.dat251.group3project.services.ItemService;
 
+import java.util.List;
+
 @SpringBootTest
 class ItemServiceTest {
 
 	@Autowired
 	private IItemRepository itemRepository;
+	private ItemService itemService;
+	private Item itemSample;
+	private Item itemSample1;
+	private Item itemSample2;
+	private Item itemSample3;
+	private Item itemSample4;
+
+
+	@BeforeEach
+	void setup() {
+		itemSample1 = new Item("Ski", "Slalomski", 1000, true);
+		itemRepository.save(itemSample1);
+		itemSample2 = new Item("Kajakk", "tomannskajakk", 1000, true);
+		itemRepository.save(itemSample2);
+		itemSample3 = new Item("Sykkel", "Terrengsykkel", 500, true);
+		itemRepository.save(itemSample3);
+		itemSample4 = new Item("Ski", "Langrenn", 100, false);
+		itemRepository.save(itemSample4);
+
+		itemService = new ItemService(itemRepository);
+	}
 
 	@Test
 	@WithMockUser("test")
 	void getAllItems() {
-		Item itemSample = new Item("Sokker", "gode gamle sokker", 1337.0, true);
-		itemRepository.save(itemSample);
-		ItemService itemService = new ItemService(itemRepository);
-		Item firstResult = itemService.findAll().get(0);
 
-		assertEquals(itemSample.getIID(), firstResult.getIID());
-		assertEquals(itemSample.getName(), firstResult.getName());
-		assertEquals(itemSample.getDescription(), firstResult.getDescription());
-		assertEquals(itemSample.getPrice(), firstResult.getPrice());
-		assertEquals(itemSample.isAvailable(), firstResult.isAvailable());
+		List<Item> itemSampleList = itemService.findAll();
+
+
+		Item firstResult = itemSampleList.get(0);
+		Item lastResult = itemSampleList.get(itemSampleList.size() - 1);
+
+		assertEquals(firstResult.getIID(), firstResult.getIID());
+		assertEquals(firstResult.getName(), itemSample1.getName());
+		assertEquals(firstResult.getDescription(), itemSample1.getDescription());
+		assertEquals(firstResult.getPrice(), 1000);
+		assertEquals(firstResult.isAvailable(), true);
+		assertEquals(lastResult.getIID(), itemSample4.getIID());
+		assertEquals(lastResult.getName(), lastResult.getName());
+		assertEquals(lastResult.getDescription(), lastResult.getDescription());
+		assertEquals(lastResult.getPrice(), lastResult.getPrice());
+		assertEquals(lastResult.isAvailable(), lastResult.isAvailable());
 	}
 
+	@Test
+	void getItemBySearchWord() {
+		List<Item> itemSampleList = itemService.findByWord("Ski");
+		List<Item> allItems = itemService.findAll();
+
+		Item resultFromAll1 = allItems.get(0);
+		Item resultFromAll2 = allItems.get(3);
+
+		Item result1 = itemSampleList.get(0);
+		Item result2 = itemSampleList.get(1);
+
+		assertEquals(result1.getIID(), resultFromAll1.getIID());
+		assertEquals(result1.getName(), "Ski");
+		assertEquals(result1.getDescription(), "Slalomski");
+		assertEquals(result1.getPrice(), 1000);
+		assertEquals(result1.isAvailable(), true);
+
+		assertEquals(result2.getIID(), resultFromAll2.getIID());
+		assertEquals(result2.getName(), "Ski");
+		assertEquals(result2.getDescription(), "Langrenn");
+		assertEquals(result2.getPrice(), 100);
+		assertEquals(result2.isAvailable(), false);
+
+	}
 }
